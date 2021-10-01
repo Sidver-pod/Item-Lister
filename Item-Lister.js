34f39e7e4528a55121d70526933ff2429631cc3b
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2021-10-02 00:34:05 +0530

    #2 getElementsByClassName();

--- Item-Lister.js
+++ Item-Lister.js
@@ -8,6 +8,8 @@
 // console.log(document.body);
 
 // console.log(document.getElementById('submit'));
+
+// #1 getElementById();
 var submitButton = document.getElementById('submit');
 submitButton.value = "SEND";
 var headerTitle = document.getElementById('header-title');
@@ -21,3 +23,17 @@ var formTitle = document.getElementById('form-title');
 formTitle.style.fontWeight = 'bold';
 formTitle.style.color = 'green';
 formTitle.style.border = 'solid 2px yellow';
+
+// #2 getElementsByClassName();
+var items = document.getElementsByClassName('list-item');
+console.log(items);
+items[1].innerText = "Namaskar";
+items[1].style.fontWeight = "bold";
+items[1].style.backgroundColor = "yellow";
+
+items[2].style.backgroundColor = "green";
+items[2].style.color = "white";
+
+for(let i=0; i<items.length; i++){
+  items[i].style.fontWeight = "bold";
+}
